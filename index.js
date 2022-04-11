ba2b761600369cdd02cdccb1833c6af0663a3a0a
gabriel Carino <gabejcarino@gmail.com>
2022-04-11 18:46:46 -0500

    commit tree 4f1ff927696cbaa3afa7159ee1337c9fd2ab2e48

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(nameInput, emailInput) {
+  const user = {
+    name : `${nameInput}`,
+    email : `${emailInput}`,
+  }
+  const configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify(user),
+  };
+  return fetch('http://localhost:3000/users', configObj)
+  .then(response => response.json())
+  .then(object => {
+    const body = document.querySelector('body');
+    body.textContent = object.id
+  })
+  .catch(error => {
+    const body = document.querySelector('body');
+    console.log(error.message);
+    body.textContent = error.message;
+  })
+};
